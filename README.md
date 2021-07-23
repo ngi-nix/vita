576419bf0effbd4abbbea3beef6be30359e6e0ff
Magic_RB <magic_rb@redalder.org>
2021-07-23 14:37:41 +0200

    Add devShell
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- README.md
+++ README.md
@@ -21,13 +21,14 @@ You can also enter a development shell with:
 $ nix develop github:ngi-nix/vita
 ```
 
-A working NixOS module is available at `thisFlake.nixosModule`.
-
 For information on how to automate this process, please take a look at [direnv](https://direnv.net/).
+
+A working NixOS module is available at `thisFlake.nixosModule`.
 
 ## Status
 
 The package itself works and is in fact fetched from upstream. The only current issue is vita not
 working in unprivileged containers which is expected nor in VMs or on any of my systems. (the two
-possible network drivers are xdp which doesn't work in containers or the intel NIC driver, which I
-assume doesn't work on non Intel platforms or NICs).
+possible network drivers are XDP, which doesn't work in containers or the intel NIC driver, which I
+assume doesn't work on non Intel platforms or NICs). I may be completely wrong about everything, 
+the documentation is sparse and confusing.
--- flake.nix
+++ flake.nix
@@ -9,112 +9,128 @@
       forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
       forAllSystems = forAllSystems' supportedSystems;
     in
-    {
-      overlays.vita = final: prev:
-        let
-          version = "master";
-          src = prev.fetchFromGitHub {
-            owner = "inters";
-            repo = "vita";
-            rev = version;
-            sha256 = "sha256-B6VNtRd3TtpFroG8Ze9fOdTrnAPJRW9EpbfvhDyusdI=";
-          };
-        in
+      with nixpkgs.lib;
+      {
+        overlays.vita = final: prev:
+          let
+            version = "master";
+            src = prev.fetchFromGitHub {
+              owner = "inters";
+              repo = "vita";
+              rev = version;
+              sha256 = "sha256-B6VNtRd3TtpFroG8Ze9fOdTrnAPJRW9EpbfvhDyusdI=";
+            };
+          in
+            {
+              vita = import (src)
+                { pkgs = prev;
+                  inherit version;
+                };
+            };
+
+        overlay = self.overlays.vita;
+
+        defaultPackage = forAllSystems (system:
+          let
+            pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.vita ]; };
+          in
+            pkgs.vita
+        );
+
+        packages = forAllSystems (system:
           {
-            vita = import (src)
-              { pkgs = prev;
-                inherit version;
-              };
+            vita = self.defaultPackage.${system};
+          }
+        );
+
+        hydraJobs = forAllSystems (system: {
+          build = self.defaultPackage.${system};
+        });
+
+        devShell = forAllSystems (system:
+          let
+            pkgs = import nixpkgs { inherit system; overlays = mapAttrsToList (_: id) self.overlays; };
+          in
+            pkgs.mkShell {} # according to the upstream derivation vita has no dependencies
+        );
+
+        nixosModule = self.nixosModules.vita;
+
+        nixosModules =
+          {
+            vita = import ./module.nix;
           };
 
-      defaultPackage = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.vita ]; };
-        in
-          pkgs.vita
-      );
+        nixosConfigurations =
+          {
+            container = nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
 
-      hydraJobs = forAllSystems (system: {
-        build = self.defaultPackage.${system};
-      });
+              modules = [
+                self.nixosModule
+                ({ ... }:
+                  {
+                    boot.isContainer = true;
+                    nixpkgs.overlays = [ self.overlays.vita ];
 
-      nixosModule = self.nixosModules.vita;
+                    services.vita = {
+                      enable = true;
 
-      nixosModules =
-        {
-          vita = import ./module.nix;
-        };
+                      instances."paris" = {
+                        xdp = false;
+                        cpus = [ 1 ];
+                        config = {
+                          private-interface4 = [
+                            {
+                              ifname = "lo";
+                              mac = "0a:cc:b1:e4:24:d2";
+                              ip = "172.31.1.20";
+                              nexthop-ip = "172.31.0.1";
+                            }
+                          ];
 
-      nixosConfigurations =
-        {
-          container = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
+                          public-interface4 = [
+                            # {
+                            #   queue = 1;
+                            #   ifname = "eth2";
+                            #   device-queue = "1";
+                            #   mac = "0a:42:13:9e:a8:ae";
+                            #   ip = "172.31.1.30";
+                            #   nat-ip = "203.0.113.2";
+                            #   nexthop-ip = "172.31.0.1";
+                            # }
 
-            modules = [
-              self.nixosModule
-              ({ ... }:
-                {
-                  boot.isContainer = true;
-                  nixpkgs.overlays = [ self.overlays.vita ];
+                            {
+                              queue = 1;
+                              ifname = "eth0@if12";
+                              device-queue = 1;
+                              mac = "0a:33:39:04:9c:ac";
+                              ip = "172.31.1.40";
+                              nat-ip = "203.0.113.3";
+                              nexthop-ip = "172.31.0.1";
+                            }
+                          ];
 
-                  services.vita = {
-                    enable = true;
+                          mtu = 1440;
 
-                    instances."paris" = {
-                      xdp = false;
-                      cpus = [ 1 ];
-                      config = {
-                        private-interface4 = [
-                          {
-                            ifname = "lo";
-                            mac = "0a:cc:b1:e4:24:d2";
-                            ip = "172.31.1.20";
-                            nexthop-ip = "172.31.0.1";
-                          }
-                        ];
-
-                        public-interface4 = [
-                          # {
-                          #   queue = 1;
-                          #   ifname = "eth2";
-                          #   device-queue = "1";
-                          #   mac = "0a:42:13:9e:a8:ae";
-                          #   ip = "172.31.1.30";
-                          #   nat-ip = "203.0.113.2";
-                          #   nexthop-ip = "172.31.0.1";
-                          # }
-
-                          {
-                            queue = 1;
-                            ifname = "eth0@if12";
-                            device-queue = 1;
-                            mac = "0a:33:39:04:9c:ac";
-                            ip = "172.31.1.40";
-                            nat-ip = "203.0.113.3";
-                            nexthop-ip = "172.31.0.1";
-                          }
-                        ];
-
-                        mtu = 1440;
-
-                        route4 = [
-                          {
-                            id = "tokyo";
-                            gateway = [
-                              { queue = 1; ip = "203.0.113.5"; }
-                              # { queue = 2; ip = "203.0.113.6"; }
-                            ];
-                            net = "172.32.0.0/16";
-                            preshared-key = "ACAB129A...";
-                            spi = 1234;
-                          }
-                        ];
+                          route4 = [
+                            {
+                              id = "tokyo";
+                              gateway = [
+                                { queue = 1; ip = "203.0.113.5"; }
+                                # { queue = 2; ip = "203.0.113.6"; }
+                              ];
+                              net = "172.32.0.0/16";
+                              preshared-key = "ACAB129A...";
+                              spi = 1234;
+                            }
+                          ];
+                        };
                       };
                     };
-                  };
-                })
-            ];
+                  })
+              ];
+            };
           };
-        };
-    };
+      };
 }
